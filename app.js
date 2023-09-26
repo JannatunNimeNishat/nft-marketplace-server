aa4e987b15517af853aac0601f7f577bf7f258a2
JannatunNimeNishat <nishatnime100@gmail.com>
2023-09-26 23:22:10 +0600

    route not found and server error middleware added

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', (req, res) => {
     res.status(200).send("nft market place is running")
 })
 
+// route not found
+app.use((req, res, next) => {
+    res.status(404).send("Route not found")
+})
 
+// server error 
+app.use((error, req, res, next) => {
+    res.status(500).send("server error", error)
+})
 
 module.exports = app;
